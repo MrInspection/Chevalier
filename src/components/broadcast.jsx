cc2bc4eebf98747a959d52bbd69def4409e28abe
Moussa <moussaabdallahhoumadi@gmail.com>
2024-04-07 21:41:57 +0200

    broadcast.jsx fix

--- src/components/broadcast.jsx
+++ src/components/broadcast.jsx
@@ -1,28 +1,28 @@
-import {InfoCircledIcon, Link1Icon, Link2Icon, LinkedInLogoIcon, NotionLogoIcon} from "@radix-ui/react-icons";
-import {Button} from "@/components/ui/button.jsx";
+import {LinkedInLogoIcon} from "@radix-ui/react-icons";
 
 export function Broadcast() {
     return (
         <>
             <div className={"pb-2"}>
                 <div className={"bg-[#e9edfe] h-full"}>
-                    <div className={"container relative h-32 md:h-20 lg:h-11 flex items-center justify-center"}>
-                        <div
-                            className={"inline-flex gap-1 items-center px-3 py-1 text-sm font-bold justify-items-center text-[#2F4077]"}>
-                            <a
-                                className={"underline"}
-                                href={"https://www.linkedin.com/posts/moussa-abdallah-houmadi_%E3%85%A4-ravi-de-partager-mon-progr%C3%A8s-avec-vous-activity-7182665154319261696-7ui5?utm_source=share&utm_medium=member_desktop"}
-                                target={"_blank"}
-                                rel="noreferrer noopener"
-                                >
+                    <a
+                        href={"https://www.linkedin.com/posts/moussa-abdallah-houmadi_%E3%85%A4-ravi-de-partager-mon-progr%C3%A8s-avec-vous-activity-7182665154319261696-7ui5?utm_source=share&utm_medium=member_desktop"}
+                        target={"_blank"}
+                        rel="noreferrer noopener"
+                    >
+                        <div className={"container relative h-32 md:h-20 lg:h-11 flex items-center justify-center"}>
+                            <div
+                                className={"inline-flex gap-1 items-center px-3 py-1 text-sm font-bold justify-items-center text-[#2F4077]"}>
+
                                 <LinkedInLogoIcon className={"invisible lg:visible"}/>
-                                La nouvelle version du portfolio est en cours de développement. Consulter le post LinkedIn
+                                La nouvelle version du portfolio est en cours de développement. Consulter le post
+                                LinkedIn
                                 Post LinkedIn pour voir la démonstration de ProjectUltron
                                 Argumentum
-                            </a>
-
+                            </div>
                         </div>
-                    </div>
+                    </a>
+
                 </div>
             </div>
         </>
