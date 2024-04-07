383e3db12fb10031219dd2eb84f1fb9c9a8a2370
Moussa <moussaabdallahhoumadi@gmail.com>
2024-04-07 21:39:02 +0200

    broadcast.jsx fix

--- src/components/broadcast.jsx
+++ src/components/broadcast.jsx
@@ -9,13 +9,18 @@ export function Broadcast() {
                     <div className={"container relative h-32 md:h-20 lg:h-11 flex items-center justify-center"}>
                         <div
                             className={"inline-flex gap-1 items-center px-3 py-1 text-sm font-bold justify-items-center text-[#2F4077]"}>
-                            <LinkedInLogoIcon className={"invisible lg:visible"}/>
-                            La nouvelle version du portfolio est en cours de développement. Consulter le <a
-                            className={"underline"}
-                            href={"https://www.linkedin.com/posts/moussa-abdallah-houmadi_%E3%85%A4-ravi-de-partager-mon-progr%C3%A8s-avec-vous-activity-7182665154319261696-7ui5?utm_source=share&utm_medium=member_desktop"}
-                            target={"_blank"}
-                            rel="noreferrer noopener">Post LinkedIn</a> pour voir la démonstration de ProjectUltron
-                            Argumentum
+                            <a
+                                className={"underline"}
+                                href={"https://www.linkedin.com/posts/moussa-abdallah-houmadi_%E3%85%A4-ravi-de-partager-mon-progr%C3%A8s-avec-vous-activity-7182665154319261696-7ui5?utm_source=share&utm_medium=member_desktop"}
+                                target={"_blank"}
+                                rel="noreferrer noopener"
+                                >
+                                <LinkedInLogoIcon className={"invisible lg:visible"}/>
+                                La nouvelle version du portfolio est en cours de développement. Consulter le post LinkedIn
+                                Post LinkedIn pour voir la démonstration de ProjectUltron
+                                Argumentum
+                            </a>
+
                         </div>
                     </div>
                 </div>
